56eb4e1eae4068f31c97b6f751a686ff71bb396b
fujiwara_k <fujiwarakai72@gmail.com>
2025-09-09 20:39:55 +0900

    1日目_ビュー作成中

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__innner">
+            <div class="header-utilities">
+                <h1 class="header__logo">FashionablyLate</h1>
+                <nav>
+                    <ul class="header-nav">
+                        @yield('header-buttons')
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
